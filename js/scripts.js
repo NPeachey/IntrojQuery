698267f734a57bf71258db1ee4f96ccd41769f49
brian nelson and neil peachey <brian@>
2018-08-06 14:49:19 -0700

    addedthe functions that rearranges the characters in our test sentence.

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,43 @@ jQuery(document).ready(function() {
 var inputSentence = prompt("type in a sentence");
 var AnnounceSentence = function(inputSentence) {
   return inputSentence;
-}
+};
 alert(inputSentence);
 
 var firstLetter = inputSentence.charAt(0).toUpperCase();
 var lastLetter = inputSentence.slice(-1).toUpperCase();
-alert(firstLetter + " " + lastLetter);
-alert(firstLetter + inputSentence.substr(1).slice(0, -1) + lastLetter);
+alert(firstLetter + lastLetter);
+alert(lastLetter + inputSentence.substr(1).slice(0, -1) + firstLetter);
 
+var chopSetence = function(inputSentence) {
+  return  inputSentence.charAt(0).toUpperCase() + inputSentence.slice(-1).toUpperCase();
+};
 
+var chopSetence = function(inputSentence) {
+  return  inputSentence.slice(-1).toUpperCase() + inputSentence.charAt(0).toUpperCase();
+};
 
+alert(chopSetence(inputSentence));
+
+alert(inputSentence + chopSetence(inputSentence));
+var countLetters = function(inputSentence) {
+  return parseInt(inputSentence.length / 2);
+};
+alert(countLetters(inputSentence));
+
+var newSentence = function(inputSentence) {
+  return inputSentence.charAt(countLetters(inputSentence));
+};
+
+var bigSentence = newSentence(inputSentence) + inputSentence + chopSetence(inputSentence);
+
+var textToRev = newSentence(bigSentence) + bigSentence + chopSetence(bigSentence);
+
+function reverse(textToRev) {
+    return textToRev.split("").reverse().join("");
+};
+
+alert(reverse(textToRev));
 
 
 
